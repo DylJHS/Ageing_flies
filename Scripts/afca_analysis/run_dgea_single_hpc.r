00072a9d5a94c890a496b41fcdea281809de89eb
DylJHS <dylanhsimmons@hotmail.fr>
2025-04-29 10:15:58 +0200

    Updated Scripts

--- Scripts/afca_analysis/run_dgea_single_hpc.r
+++ Scripts/afca_analysis/run_dgea_single_hpc.r
@@ -4,11 +4,12 @@ library(Seurat)
 library(zellkonverter)
 library(dplyr)
 library(fs)
+library(MAST)
 
 # Get arguments
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 2) {
-  stop("Usage: run_dgea_single.R <input_file> <output_dir>")
+  stop("Usage: run_dgea_single_hpc.r <input_file> <output_dir>")
 }
 input_file <- args[1]
 output_dir <- args[2]
@@ -22,6 +23,7 @@ if (!file.exists(input_file)) {
 file_name <- path_file(input_file)
 ct_name <- path_ext_remove(file_name)
 results_file <- file.path(output_dir, paste0("markers_", ct_name, ".csv"))
+cat(paste("\t\tRunning DGEA on:", ct_name, "\n"))
 
 # Read and convert
 suppressWarnings({
@@ -34,11 +36,13 @@ rm(ad)
 adata_Seurat$age <- as.factor(adata_Seurat$age)
 adata_Seurat$sex_age <- as.factor(adata_Seurat$sex_age)
 adata_Seurat$afca_annotation <- as.factor(adata_Seurat$afca_annotation)
+adata_Seurat$indiv <- as.factor(adata_Seurat$indiv)
 
 # Define groups
 young_group <- "5"
 groups_present <- unique(adata_Seurat$age)
 other_groups <- setdiff(groups_present, young_group)
+
 
 # Run DGEA
 res_list <- list()
@@ -46,18 +50,27 @@ if (young_group %in% groups_present) {
   for (grp in other_groups) {
     if (grp %in% groups_present) {
       cat(paste("  Running DGEA for:", young_group, "vs", grp, "\n"))
-      markers <- FindMarkers(adata_Seurat,
-                             ident.1 = young_group,
-                             ident.2 = grp,
-                             group.by = "age",
-                             test.use = "wilcox")
-
+      markers <- FindMarkers(
+        adata_Seurat,
+        ident.1 = young_group,
+        ident.2 = grp,
+        group.by = "age",
+        test.use = "MAST", # Using MAST to account for batch effects
+        latent.vars = "indiv" # Adjust for individual variability
+      ) 
+      
+      # Check markers
+      cat("Shape of markers:", paste(dim(markers), collapse = " x "), "\n")
+      # print the first few rows of markers
+      print(head(markers))
+      
+      
       markers$cell_type <- ct_name
       markers$comparison <- paste(young_group, "vs", grp)
       markers$gene <- rownames(markers)
-      markers <- markers %>% rename(p_val_BF = p_val_adj)
+      # markers <- markers %>% rename(p_val_BF = p_val_adj)
       markers$p_val_adj <- p.adjust(markers$p_val, method = "BH")
-
+      
       res_list[[grp]] <- markers
     }
   }
@@ -68,3 +81,4 @@ if (length(res_list) > 0) {
   all_results <- do.call(rbind, res_list)
   write.table(all_results, results_file, sep = ",", row.names = FALSE, col.names = TRUE)
 }
+cat("\n\nFinished Running DGEA on Cell Type !\n")
