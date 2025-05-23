f29ee82ea6ef59bd90ad580ee31f230d5a721d40
DylJHS <dylanhsimmons@hotmail.fr>
2025-05-23 12:52:25 +0200

    changes to the random scripts

--- Scripts/random_search.py
+++ Scripts/random_search.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import os
 import sys
+import anndata as ad
 
-name_list = ['AAACCCAAGGATACAT-1_AFCA_female_body_30_S1',
-       'AAACCCAGTGCAATAA-1_AFCA_female_body_30_S1',
-       'AAACCCATCCGATGTA-1_AFCA_female_body_30_S1',
-       'AAACCCATCGTGGGTC-1_AFCA_female_body_30_S1']
 
-print(name_list)\
+directory = "/hpc/shared/onco_janssen/dhaynessimmons/projects/ageing_flies/data/ct_specific"
+
+for file in os.listdir(directory):
+    if file.endswith(".h5ad"):
+        print(file)
+        adata = ad.read_h5ad(os.path.join(directory, file))
+        print(adata.obs.iloc[-3:].head(), '\n\n')
